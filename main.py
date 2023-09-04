75e6445841c575a40b8eacad3a5a3d029ff12fec
Daudu Arunegbode charles <blaqwayschannel@gmail.com>
2023-09-04 20:32:15 +0100

    basic server set up with flask

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask , request, render_template, redirect, url_for
+from dotenv import load_dotenv
+import os
+
+load_dotenv();
+
+app = Flask(__name__);
+
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
