eba170336fdfcdc743b197f714ab2540c82a1c4e
Daudu Arunegbode charles <blaqwayschannel@gmail.com>
2023-09-12 20:47:15 +0100

    added waitress package to serve even in production

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+from waitress import serve
 from app import *
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    # app.run(debug=True) 
+    app.debug = True
+    # app.run(host= "0.0.0.0", port= 5050)  this is for developent alone
+    serve(app,host= "0.0.0.0", port= 5050) 
