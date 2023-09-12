2d103cee7292acaa0ae4876711ff98111d77f18f
Daudu Arunegbode charles <blaqwayschannel@gmail.com>
2023-09-12 20:29:45 +0100

    added a clound mongo db url and connection

--- app.py
+++ app.py
@@ -3,18 +3,15 @@ from dotenv import load_dotenv
 import pymongo
 import os
 
-# import jwt
-# from flask_bcrypt import Bcrypt 
-# from flask_jwt_extended import JWTManager,
 load_dotenv();
 
 
 app         = Flask(__name__);
 app.config['SECRET_KEY'] = os.getenv("SECRET-KEY");
-client      = pymongo.MongoClient('localhost', 27017)
-db          = client.login_system_DB
+client      = pymongo.MongoClient(os.getenv('MONGO_URI_ATLAS'))
+# db          = client.login_system_DB  local database
+db          = client.login_system_python_DB
 
-# jwt_extd= JWTManager(app)
 
 # ROUTES
 from user.routes import *
